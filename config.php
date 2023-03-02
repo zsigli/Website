af710a03fc99b690b0bebd76f0abcb47ad2f32e2
zsigli <119347918+zsigli@users.noreply.github.com>
2023-03-02 15:06:12 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+    extract($_REQUEST);
+    $file=fopen("form-save.txt","a");
+
+    fwrite($file,"name :");
+    fwrite($file, $username ."\n");
+    fwrite($file,"Email :");
+    fwrite($file, $email ."\n");
+    fwrite($file,"Password :");
+    fwrite($file, $password ."\n");
+    fclose($file);
+    header("location: index.html");
+    fread($file, $username ."\n")
+ ?>
